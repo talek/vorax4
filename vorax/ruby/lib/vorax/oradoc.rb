5634e0bce53cfd91a0e7297a1f18461219c5a4df
Alexandru Tica <alexandru.tica@gmail.com>
2013-10-17 18:27:34 +0300

    Quick fix for CS mismatch

--- vorax/ruby/lib/vorax/oradoc.rb
+++ vorax/ruby/lib/vorax/oradoc.rb
@@ -13,6 +13,7 @@ module Vorax
 	module Oradoc
 
 		def self.all_books(doc_folder)
+			nbsp = Nokogiri::HTML("&nbsp;").text
 			folder = Pathname.new(doc_folder).realpath
 			books_list = []
 			Dir["#{folder}/**/index.htm"].each do |index_file|
@@ -20,6 +21,7 @@ module Vorax
 					doc = Nokogiri::HTML(file)
 					book = doc.at("meta[@name='doctitle']/@content").to_s
 					book.gsub!(/^Oracle[^[:ascii:]] /, '')
+					book.gsub!(nbsp, " ")
 					unless book.empty?
 						books_list << book
 						yield(book, index_file) if block_given?
@@ -31,6 +33,7 @@ module Vorax
 
 		def self.create_index(doc_folder, index_folder, selective_books = nil)
 			folder = Pathname.new(doc_folder).realpath
+			nbsp = Nokogiri::HTML("&nbsp;").text
 			filter = "."
 			if selective_books && !selective_books.empty?
 				filter = selective_books.map { |b| "^(#{Regexp.escape(b)})" }.join('|')
@@ -48,6 +51,7 @@ module Vorax
 							return if Oradoc::abort?
 							doc = Nokogiri::HTML(file)
 							title = doc.at("head/title").content
+							text.gsub(nbsp, " ")
 							index.add_document :file => path, 
 								:content => doc.text.split,
 								:title => title.to_s,
@@ -62,13 +66,16 @@ module Vorax
 		end
 
 		def self.search(index_folder, what)
+			nbsp = Nokogiri::HTML("&nbsp;").text
 			index = Index::Index.new(:path => index_folder, 
 															:id_field => 'content',
 															:create => false)
 			results = []
 			index.search_each(what, :limit => :all) do |doc, score| 
-				results << {:title => index[doc]['title'],
-					:book => index[doc]['book'],
+				title = index[doc]['title'].force_encoding(Encoding::UTF_8)
+				book = index[doc]['book'].force_encoding(Encoding::UTF_8)
+				results << {:title => title.gsub(nbsp, " "),
+					:book => book.gsub(nbsp, " "),
 					:file => index[doc]['file'],
 					:score => score}
 			end
